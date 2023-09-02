216b8ab3378da843df1c55f6201bdf55f9a7fa43
sitandra <u.deb@mail.ru>
2023-09-02 15:04:27 +0500

    Initial Commit

---
+++ obj-c-2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  obj-c-2
+//
+//  Created by Юлия Дебелова on 02.09.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
